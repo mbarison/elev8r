82a55b7a2b60450ba8d1128546c206fdbf17f3d6
mbarison <eltorpe@hotmail.com>
2020-08-27 13:18:44 -0400

    Fit Erlang distribution

---
+++ erlang_fit.py
@@ -0,0 +1,54 @@
+'''
+Created on 27-Aug-2020
+
+@author: mbarison
+
+
+Fit Erlang distribution (k=1, exponential) to time arrival data
+
+'''
+
+def erlang_fit(df, nbins=50):
+
+    tmp=[]
+    
+    print("Data points=%d" % len(df))
+    
+    for i in range(1, len(df)):
+        if df["run"].iloc[i] == df["run"].iloc[i-1]:
+            dt = df["arrival"].iloc[i]-df["arrival"].iloc[i-1]
+            if dt >= 0:                        
+                tmp.append(dt)
+
+    dfp = pd.Series(tmp)
+
+    #print("Diff points=%d" % len(dfp))
+
+    ax = plt.gca()
+    ax.set_yscale('log')
+    bin_heights, bin_borders, _ = plt.hist(dfp, bins=nbins)
+    
+    bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2
+    
+    def erlang(x, lamb, scale):
+        return scale * np.exp(-lamb*x)
+    
+    popt, pcov = curve_fit(erlang, bin_centers, bin_heights, p0=[1., bin_heights[0]])
+    perr = np.sqrt(np.diag(pcov))
+    
+    
+    residuals = bin_heights - erlang(bin_centers, *popt)
+    ss_res = np.sum(residuals**2)
+    ss_tot = np.sum((bin_heights-np.mean(bin_heights))**2)
+
+    r_squared = 1 - (ss_res / ss_tot)
+
+    
+    print("Fit results: lambda=%g scale=%g" % (popt[0], popt[1]))
+    print("Fit error: eps_lambda=%g eps_scale=%g" % (perr[0], perr[1]))
+    print("R-squared=%g" % r_squared)
+    
+    x_interval_for_fit = np.linspace(bin_borders[0], bin_borders[-1], 10000)
+
+    plt.plot(x_interval_for_fit, erlang(x_interval_for_fit, *popt), label='fit', c='red')
+    
